800d5bac897cae756339694690bfb3b4c293eeea
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-10 22:51:46 +0300

    Edit Routes

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-export default function Card({products}) {
-  return (
-      products.map(({id, img_url, name, price, category}) => {     
-    return <div key={id.toString()} className="card">
-        <img src={img_url} alt="product img" width="150px" height="150px"/>
+export default function Card({ products }) {
+  return products.map(({ id, img_url, name, price, category }) => {
+    return (
+      <div key={id.toString()} className="card">
+        <img src={img_url} alt="product img" width="150px" height="150px" />
         <h3>{name}</h3>
         <p>{price}</p>
         <p>{category}</p>
         <button>DELETE</button>
         <button>EDIT</button>
-    </div>
-      })
-  )
+      </div>
+    );
+  });
 }
