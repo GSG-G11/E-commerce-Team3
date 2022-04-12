95a4286f1751542667757855f459f2a3de4a1841
rawaneyad <rawaneyad20@gmail.com>
2022-04-12 11:34:24 +0300

    Style

--- client/src/components/MealDetails/MealDetails.jsx
+++ client/src/components/MealDetails/MealDetails.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
+import './mealDetails.css'
 export default function MealDetails({mealDetails}) {
   const {  img_url, name, price, category, description }= mealDetails;
   return (
-    <div >
-        <img src={img_url} alt="meal img" width="150px" height="150px" />
-        <h3>{name}</h3>
-        <p>{price}</p>
-        <p>{category}</p>
-        <p>{description}</p>
+    <div className='mealDetails'>
+        <img src={img_url} alt="meal img" width="150px" height="150px" className='mealimg'/>
+        <div className="DataSide">
+          <h3>{name}</h3>
+          <p>{price}</p>
+          <p>{category}</p>
+          <p>{description}</p>
+        </div>
     </div>
   )
 }
